9b3779ecc2210f91abef50e12003f980fac1ae4d
LCBlakey24-alt <260519050+LCBlakey24-alt@users.noreply.github.com>
2026-07-04 22:42:46 +0100

    Restore medicine skill ability

--- frontend/src/components/clean-sheet/cleanSheetUtils.js
+++ frontend/src/components/clean-sheet/cleanSheetUtils.js
@@ -13,7 +13,7 @@ export const SKILLS = [
   ['Acrobatics', 'dexterity'], ['Animal Handling', 'wisdom'], ['Arcana', 'intelligence'],
   ['Athletics', 'strength'], ['Deception', 'charisma'], ['History', 'intelligence'],
   ['Insight', 'wisdom'], ['Intimidation', 'charisma'], ['Investigation', 'intelligence'],
-  ['Medicine', 'nature'], ['Nature', 'intelligence'], ['Perception', 'wisdom'],
+  ['Medicine', 'wisdom'], ['Nature', 'intelligence'], ['Perception', 'wisdom'],
   ['Performance', 'charisma'], ['Persuasion', 'charisma'], ['Religion', 'intelligence'],
   ['Sleight of Hand', 'dexterity'], ['Stealth', 'dexterity'], ['Survival', 'wisdom'],
 ];
